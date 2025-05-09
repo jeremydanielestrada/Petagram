8d80a950f5ee72093e0f12faf96c1bd7609f7ff6
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-05-09 19:03:12 +0800

    feat: added form

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <v-form>
+    <v-text-field v-model="firstName" :rules="firstNameRules" label="First name"></v-text-field>
+
+    <v-text-field v-model="lastName" :rules="lastNameRules" label="Last name"></v-text-field>
+
+    <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+  </v-form>
+</template>
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+</script>
 
-<template></template>
+<template>
+  <v-card>
+    <v-card-title>
+      <p>Logo</p>
+    </v-card-title>
+    <v-card-text> </v-card-text>
+  </v-card>
+</template>
 
 <style scoped></style>
